7686e7af1f98a6f1fae99ace2186f974a1ebfee6
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2017-03-17 19:02:37 +0000

    use default thread pool size

--- java_spark/src/main/java/server/SparkServer.java
+++ java_spark/src/main/java/server/SparkServer.java
@@ -13,12 +13,14 @@ import org.slf4j.LoggerFactory;
 public class SparkServer {
 	  public static final Logger logger = LoggerFactory.getLogger(SparkServer.class);
 	  public static final int PORT=8080;
-	  public static final int THREADS=Runtime.getRuntime().availableProcessors();
+	  public static final int MIN_THREADS=6;
+	  public static final int MAX_THREADS=8;
+	  public static final int THREAD_POOL_TIMEOUT = 30000;
     public static void main(String[] args) {
-        threadPool(THREADS);
+        //threadPool(MAX_THREADS, MIN_THREADS,THREAD_POOL_TIMEOUT);
         port(PORT);
         webSocket("/", EchoWebSocket.class);
-				logger.info("==> starting echo server at port:"+PORT + " with threadpool size:"+ THREADS);
+				logger.info("==> starting echo server at port:"+PORT);
 			  init();
     }
 }
