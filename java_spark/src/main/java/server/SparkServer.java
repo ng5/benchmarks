2787527db14fc7836025e81206fb8d97d9ac5b83
Rohit Vishwakarma <inbox.rohitv@gmail.com>
2017-03-17 18:55:25 +0000

    added threadpool size and port number as constant

--- java_spark/src/main/java/server/SparkServer.java
+++ java_spark/src/main/java/server/SparkServer.java
@@ -3,15 +3,22 @@ package server;
 
 import static spark.Spark.init;
 import static spark.Spark.port;
+import static spark.Spark.threadPool;
 import static spark.Spark.webSocket;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  * Created by rohitv on 09/03/17.
  */
 public class SparkServer {
+	  public static final Logger logger = LoggerFactory.getLogger(SparkServer.class);
+	  public static final int PORT=8080;
+	  public static final int THREADS=Runtime.getRuntime().availableProcessors();
     public static void main(String[] args) {
-        port(8080);
+        threadPool(THREADS);
+        port(PORT);
         webSocket("/", EchoWebSocket.class);
-        init();
+				logger.info("==> starting echo server at port:"+PORT + " with threadpool size:"+ THREADS);
+			  init();
     }
 }
